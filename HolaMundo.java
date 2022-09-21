4749393d25906337327e1cb3775064766bd90137
Sion Cerezo Juarez <syon.dev@outlook.com>
2022-09-21 18:40:56 -0500

    cambio en dev

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,5 @@
 public class HolaMundo {
     public static void main(String... args){
-        System.out.println("Hola Mundo desde Openshift");
+        System.out.println("Hola Mundo desde Openshift / Dev environment");
     }
 }
